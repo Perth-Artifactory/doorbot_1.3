dac800ee5fc112aef30ae8b3a04a47679c577d8f
tazard <16077190+tazard@users.noreply.github.com>
2021-04-04 16:20:08 +0800

    Start new main

--- main.py
+++ main.py
@@ -1,88 +1,26 @@
 #!/usr/bin/python3
 
-import serial
-import json
-import time
-import sys
-import os
-import requests
-import random
-from requests.auth import HTTPBasicAuth
-# from slackclient import SlackClient
 
-ENCLAVE_URL_CONFIG = 'https://enclave.guthrie.artifactory.org.au/door/config.json'
-ENCLAVE_URL_KEYS = 'https://enclave.guthrie.artifactory.org.au/door/keys.json'
-# HTTP Auth username/password stored in "config/auth_config.json".
-# See "config/auth_config-example.json" for the format. Request current user/password
-# from active maintainers of doorbot.
+class Doorbot:
+    def __init__(self):
+        # Load config
+        # Init Doorbot Hat Interface
+        # Init RFID reader
+        # Init slack interface
+        # Init audio interface
+        pass
 
-auth = HTTPBasicAuth(ENCLAVE_AUTH_USER, ENCLAVE_AUTH_PASS)
+    def run(self):
+        while True:
+            # Check inputs
+            # Callback handlers process events
+            pass
 
-
-def load_config():
-    """Loads configuration from disk and checks for update from enclave"""
-
-    config = requests.get('https://enclave.guthrie.artifactory.org.au/door/config.json', auth=HTTPBasicAuth('doorbot', 'Kn0ckKn0ck')).json()
-    keys = requests.get('https://enclave.guthrie.artifactory.org.au/door/keys.json', auth=HTTPBasicAuth('doorbot', 'Kn0ckKn0ck')).json()
-    active_members = []
-    members = []
-    for x in keys:
-	# Number corresponds to access level. 0 = no access, 1 = standard key member, 3 = committee
-        if keys[x]["door"] > 0:
-            active_members.append(x)
-        members.append(x)
-    return (config,keys,active_members,members)
-
-
-def relay_init():
-    """Initialise relays using serial settings from config"""
-    return serial.Serial(config["serial"]["pointer"], baudrate=config["serial"]["baudrate"])
-
-def unlock_door(relays, t=5):
-    """Unlock door for t seconds"""
-    relays.write('A'.encode())
-    time.sleep(t)
-    relays.write('a'.encode())
-
-def speaker(sound):
-    """Play requested sound, if found in config."""
-    if config["sounds"].get(sound, ''):
-        os.system ("mpg123 -q {} &".format(config["sounds"][sound]))
-    else:
-        notify(door_activity=False,status="Nonexistent sound '{}' requested".format(sound))
-        #Play default
-        sound = "granted"
-        os.system ("mpg123 -q {} &".format(config["sounds"][sound]))
-
-def notify(door_activity=False,id=" ",name=" ",status=" ",level=1):
-    if status == "Unlocked" and name == " ":
-        name = "UNNAMED KEY, IDENTIFY USER"
-    if door_activity:
-        print("notifying slack")
-        attachments = [{"fallback":"An image of the front door",
-                          "image_url":"http://space.artifactory.org.au/foyer.gif"},
-                       {"fallback":"An image of the carpark",
-                          "image_url":"http://space.artifactory.org.au/carpark.gif",
-                          "fields":[{"title":"Name",
-                   "value":name,
-                   "short":True},
-                  {"title":"RFID",
-                   "value":id,
-                   "short":True},
-                  {"title":"Status",
-                   "value":status,
-                   "short":True},
-                  {"title":"Level",
-                   "value":level,
-                   "short":True}]}]
-        try:
-            slack.api_call("chat.postMessage",channel=config["slack"]["channel"],text="Someone interacted with the door",attachments=attachments)
-        except:
-            print("couldn't reach slack")
-    else:
-        print(status)
 
 def main():
+
+
+
     # load config
 
     config,keys,good_members,members = load_config()
