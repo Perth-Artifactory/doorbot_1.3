69148e1ba3514c63288f3ac9584da42e7cadc6e9
Blake Samuels <16077190+tazard@users.noreply.github.com>
2022-10-03 21:17:06 +0800

    Start to port across doorSlack into home app

--- app.py
+++ app.py
@@ -1,70 +1,77 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-import os
-from slack_bolt.app.async_app import AsyncApp
+from readline import get_begidx
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
+from slack_bolt.app.async_app import AsyncApp
+import os
+import logging
+import slack_blocks
+logging.basicConfig(level=logging.DEBUG)
 
 app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 
+
+
+def check_auth(b):
+    """Check whether a user/channel is authorised"""
+    if b['user']['id'] == 'U7DD219GF':  # tazard
+        return True
+    return False
+
+def get_response_value(b):
+    """Find the value action payload from dropdown"""
+    for block in b['actions']:
+        if 'selected_option' in block:
+            return block['selected_option']['value']
+        else:
+            return block['value']
+
 @app.event("app_home_opened")
 async def update_home_tab(client, event, logger):
-  try:
-    # views.publish is the method that your app uses to push a view to the Home tab
-    await client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        await client.views_publish(
+            # the user that opened your app's app home
+            user_id=event["user"],
+            # the view object that appears in the app home
+            view=slack_blocks.home_view
+        )
 
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    )
-  
-  except Exception as e:
-    logger.error(f"Error publishing home tab: {e}")
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
 
-@app.action("qPFv")
-async def handle_some_action(ack, body, logger):
+@app.action("sendMessage")
+async def handle_send_message(ack, body, logger):
     await ack()
-    logger.info(body)
-    
+    logger.info("app.action 'sendMessage':" + str(body))
+    if check_auth(body):
+        value = get_response_value(body)
+        logger.info(f"SEND MESSAGE = {value}")
+        messages = {"key_disabled":"noticeDisabled",
+                    "volunteer_contact":"noticeContact",
+                    "covid":"noticeCOVID",
+                    "notice_you":"noticePresence"}
+        # value = findValue(body,"sendMessage")
+        # say("<@{}> sent the predefined message '{}'".format(body['user']['id'], value))
+        # play(messages[value],OS="WIN")
+
+@app.action("ttsMessage")
+async def handle_tts_message(ack, body, logger):
+    await ack()
+    logger.info("app.action 'ttsMessage':" + str(body))
+    if check_auth(body):
+        value = get_response_value(body)
+        logger.info(f"TTS MESSAGE = {value}")
+
+@app.action("unlock")
+async def handle_unlock(ack, body, logger):
+    await ack()
+    logger.info("app.action 'unlock':" + str(body))
+    if check_auth(body):
+        value = get_response_value(body)
+        logger.info(f"DOOR UNLOCK = {value} seconds")
+
 async def run_offline_task():
     while True:
-        channel_id = "#doorbot-slack-test"  # Blake
+        channel_id = "#doorbot-slack-test"
         await app.client.chat_postMessage(
             channel=channel_id,
             text="Hi there!",
---
+++ slack_blocks.py
@@ -0,0 +1,103 @@
+"""
+Separate file for blockkit blocks because they are huge and make code
+hard to follow.
+"""
+
+# The Slack Home Tab
+home_view = {
+    "type": "home",
+    "callback_id": "home_view",
+
+    # body of the view
+    "blocks":
+    [
+        {
+            "type": "actions",
+            "elements":
+            [
+                {
+                    "type": "static_select",
+                    "placeholder":
+                    {
+                        "type": "plain_text",
+                        "text": "Select a message to send",
+                        "emoji": True
+                    },
+                    "options":
+                    [
+                        {
+                            "text":
+                            {
+                                "type": "plain_text",
+                                "text": "Your key has been disabled",
+                                "emoji": True
+                            },
+                            "value": "key_disabled"
+                        },
+                        {
+                            "text":
+                            {
+                                "type": "plain_text",
+                                "text": "A volunteer will contact you shortly",
+                                "emoji": True
+                            },
+                            "value": "volunteer_contact"
+                        },
+                        {
+                            "text":
+                            {
+                                "type": "plain_text",
+                                "text": "COVID shutdown notice",
+                                "emoji": True
+                            },
+                            "value": "covid"
+                        },
+                        {
+                            "text":
+                            {
+                                "type": "plain_text",
+                                "text": "I know you're there",
+                                "emoji": True
+                            },
+                            "value": "notice_you"
+                        }
+                    ],
+                    "action_id": "sendMessage"
+                }
+            ]
+        },
+        {
+            "dispatch_action": True,
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "ttsMessage"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Send a custom message via text-to-speech",
+                "emoji": True
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Manually unlock the door, a sound notification will be played automatically"
+            },
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Unlock for 30 seconds",
+                    "emoji": True
+                },
+                "value": "30",
+                "action_id": "unlock"
+            }
+        }
+    ]
+}
