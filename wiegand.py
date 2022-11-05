e4ec5be736abdb87cacf6cb3c477b40b4ad6695c
Blake Samuels <16077190+tazard@users.noreply.github.com>
2022-11-05 21:02:45 +0800

    Split out example use and cleanup

--- wiegand.py
+++ wiegand.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+Class to read Wiegand using pigpio
+
+From https://github.com/joan2937/pigpio/blob/master/EXAMPLES/Python/WIEGAND_CODE/wiegand.py
+"""
+
 import pigpio
 
 class decoder:
@@ -7,7 +13,7 @@ class decoder:
    """
    A class to read Wiegand codes of an arbitrary length.
 
-   The code length and value are returned.
+   The code length and value are returned via callback.
 
    EXAMPLE
 
@@ -69,9 +75,6 @@ class decoder:
       """
 
       if level < pigpio.TIMEOUT:
-         gpio_s = {True: 'gpio_0', False: 'gpio_1'}[gpio == self.gpio_0]
-         # print(f"{gpio_s=}, {level=}, {tick=} us", end="")
-
          if self.in_code == False:
             self.bits = 1
             self.num = 0
@@ -81,27 +84,24 @@ class decoder:
             self.pi.set_watchdog(self.gpio_0, self.bit_timeout)
             self.pi.set_watchdog(self.gpio_1, self.bit_timeout)
 
-            # print()
-            self.last_tick = tick
          else:
             self.bits += 1
             self.num = self.num << 1
 
-            # print(f", Delta={tick - self.last_tick} us")
-            self.last_tick = tick
-
          if gpio == self.gpio_0:
-            self.code_timeout = self.code_timeout & 2 # clear gpio 0 timeout
+            # Clear gpio 0 timeout
+            self.code_timeout = self.code_timeout & 2 
          else:
-            self.code_timeout = self.code_timeout & 1 # clear gpio 1 timeout
+            # Clear gpio 1 timeout
+            self.code_timeout = self.code_timeout & 1 
             self.num = self.num | 1
 
       else:
 
          if self.in_code:
-
             if gpio == self.gpio_0:
-               self.code_timeout = self.code_timeout | 1 # timeout gpio 0
+               # Add timeout for gpio 0
+               self.code_timeout = self.code_timeout | 1 
             else:
                self.code_timeout = self.code_timeout | 2 # timeout gpio 1
 
@@ -120,53 +120,3 @@ class decoder:
       self.cb_0.cancel()
       self.cb_1.cancel()
 
-if __name__ == "__main__":
-
-   import time
-
-   import pigpio
-
-   import wiegand
-
-   def callback(bits, value):
-      if bits == 26:
-         # Parity checking from here:
-         # https://github.com/paulo-raca/YetAnotherArduinoWiegandLibrary/blob/master/src/Wiegand.cpp
-
-         # The trailing half of the bits are odd parity (including parity bit).
-         # Trailing half is what arrives last - the low bits.
-         count_trailing = 0
-         for i in range(0, bits//2):
-            if (value >> i) & 0x01 == 0x01:
-               count_trailing += 1
-         trailing_parity_odd = (count_trailing % 2 == 1)
-
-         # Leading half is even parity (high bits)
-         count_leading = 0
-         for i in range(bits//2, bits):
-            if (value >> i) & 0x01 == 0x01:
-               count_leading += 1
-         leading_parity_even = (count_leading % 2 == 0)
-
-         if trailing_parity_odd and leading_parity_even:
-            # Extract the card value from inner 24-bits (drop first and last parity bits)
-            card_id = (value >> 1) & (2**(bits-2)-1)
-            print(f"VALID CARD ({bits-2}): {card_id}, 0x{card_id:0X}")
-         else:
-            print(f"ERROR: Invalid Parity - dec {value}, 0x{value:0X}, {count_trailing=} ({trailing_parity_odd=}), {count_leading=} ({leading_parity_even=})")
-         
-      else:
-         print("bits={} value={}".format(bits, value))
-
-   pi = pigpio.pi()
-
-   w_rfid = wiegand.decoder(pi, 5, 6, callback)
-
-   w_nfc = wiegand.decoder(pi, 12, 13, callback)
-
-   time.sleep(300)
-
-   w_rfid.cancel()
-   w_nfc.cancel()
-
-   pi.stop()
---
+++ wiegand_test.py
@@ -0,0 +1,52 @@
+"""
+A standalone example using weigand class to read both RFID and NFC keys
+using Doorbot 1.3 hat.
+"""
+
+import time
+import pigpio
+import wiegand
+
+if __name__ == "__main__":
+    def callback(bits, value):
+        if bits == 26:
+            # Parity checking from here:
+            # https://github.com/paulo-raca/YetAnotherArduinoWiegandLibrary/blob/master/src/Wiegand.cpp
+
+            # The trailing half of the bits are odd parity (including parity bit).
+            # Trailing half is what arrives last - the low bits.
+            count_trailing = 0
+            for i in range(0, bits//2):
+                if (value >> i) & 0x01 == 0x01:
+                    count_trailing += 1
+            trailing_parity_odd = (count_trailing % 2 == 1)
+
+            # Leading half is even parity (high bits)
+            count_leading = 0
+            for i in range(bits//2, bits):
+                if (value >> i) & 0x01 == 0x01:
+                    count_leading += 1
+            leading_parity_even = (count_leading % 2 == 0)
+
+            if trailing_parity_odd and leading_parity_even:
+                # Extract the card value from inner 24-bits (drop first and last parity bits)
+                card_id = (value >> 1) & (2**(bits-2)-1)
+                print(f"VALID CARD ({bits-2}): {card_id}, 0x{card_id:0X}")
+            else:
+                print(f"ERROR: Invalid Parity - dec {value}, 0x{value:0X}, {count_trailing=} ({trailing_parity_odd=}), {count_leading=} ({leading_parity_even=})")
+         
+        else:
+            print("bits={} value={}".format(bits, value))
+
+    pi = pigpio.pi()
+
+    w_rfid = wiegand.decoder(pi, 5, 6, callback)
+
+    w_nfc = wiegand.decoder(pi, 12, 13, callback)
+
+    time.sleep(300)
+
+    w_rfid.cancel()
+    w_nfc.cancel()
+
+    pi.stop()
