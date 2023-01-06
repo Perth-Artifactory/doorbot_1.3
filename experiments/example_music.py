d767e5bcbbeb1aafe57444734336e06510ace7c8
Blake Samuels <16077190+tazard@users.noreply.github.com>
2023-01-06 12:02:47 +0800

    Add more music tests

--- experiments/example_music.py
+++ experiments/example_music.py
@@ -1,8 +1,18 @@
 # https://raspberrypi.stackexchange.com/questions/7088/playing-audio-files-with-python
+# 
+# To get around 
+# NotImplementedError: mixer module not available (ImportError: libSDL2_mixer-2.0.so.0: cannot open shared object file: No such file or directory)
+# > sudo apt-get install python3-sdl2
 
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("sounds/denied.mp3")
+pygame.mixer.music.load("sounds/granted.mp3")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
+
+import time
+time.sleep(0.5)
+
+# import os
+# os.system ("mpg123 -q {} &".format("sounds/granted.mp3"))
