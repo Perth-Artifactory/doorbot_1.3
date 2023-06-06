799e1d8ac38d8697c6bc50a5facb492330d0d956
Blake Samuels <16077190+tazard@users.noreply.github.com>
2023-06-06 22:03:04 +0800

    Add door and screen out door=0

--- doorbot/utils/name_association.py
+++ doorbot/utils/name_association.py
@@ -31,39 +31,49 @@ def name_associations(tidyhq, legacy, output_file):
     # Find best match for each name in tidyhq_dict from legacy_dict
     for tidyhq_id, tidyhq_value in tidyhq_dict.items():
         tidyhq_name = tidyhq_value["name"]
+        tidyhq_door = tidyhq_value.get('door', None)
         legacy_name = best_match(tidyhq_name, legacy_dict)
         if legacy_name:
             # Find the corresponding id in legacy_dict
             legacy_id = [id for id, value in legacy_dict.items() if value["name"] == legacy_name][0]
+            legacy_door = legacy_dict[legacy_id].get('door', None)
             matched_legacy_names.append(legacy_name)
             if tidyhq_id == legacy_id:
                 id_match.append({
                     "tidyhq_id": tidyhq_id, 
                     "tidyhq_name": tidyhq_name, 
+                    "tidyhq_door": tidyhq_door,
                     "legacy_id": legacy_id, 
-                    "legacy_name": legacy_name
+                    "legacy_name": legacy_name,
+                    "legacy_door": legacy_door
                 })
             else:
                 id_different.append({
                     "tidyhq_id": tidyhq_id, 
                     "tidyhq_name": tidyhq_name, 
+                    "tidyhq_door": tidyhq_door,
                     "legacy_id": legacy_id, 
-                    "legacy_name": legacy_name
+                    "legacy_name": legacy_name,
+                    "legacy_door": legacy_door
                 })
         else:
             tidyhq_without_match.append({
                 "tidyhq_id": tidyhq_id,
-                "tidyhq_name": tidyhq_name
+                "tidyhq_name": tidyhq_name,
+                "tidyhq_door": tidyhq_door
             })
 
     # Find legacy names without a match in tidyhq
     for legacy_id, legacy_value in legacy_dict.items():
-        legacy_name = legacy_value["name"]
-        if legacy_name not in matched_legacy_names:
-            legacy_without_match.append({
-                "legacy_id": legacy_id,
-                "legacy_name": legacy_name
-            })
+        if legacy_value.get('door', 1) != 0:  # Ignore if door equals 0
+            legacy_name = legacy_value["name"]
+            legacy_door = legacy_value.get('door', None)
+            if legacy_name not in matched_legacy_names:
+                legacy_without_match.append({
+                    "legacy_id": legacy_id,
+                    "legacy_name": legacy_name,
+                    "legacy_door": legacy_door
+                })
 
     # Write associations to output file
     associations = {"id_match": id_match, "id_different": id_different, 
