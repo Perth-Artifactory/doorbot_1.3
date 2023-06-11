4afdf52940054e7ff5ebde69ce0c133988f4ab43
Blake Samuels <16077190+tazard@users.noreply.github.com>
2023-06-11 20:49:06 +0800

    Improve matching further

--- doorbot/utils/name_association.py
+++ doorbot/utils/name_association.py
@@ -1,14 +1,24 @@
+from fuzzywuzzy import fuzz, process
 import json
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
-def best_match(name, dict_names, score_cutoff=80):
-    """Return the best match to the name from dict_names.
-    Only return matches with a score above score_cutoff."""
-    names_list = [v["name"] for v in dict_names.values()]
-    best_match = process.extractOne(name, names_list, scorer=fuzz.token_sort_ratio)
-    if best_match and best_match[1] > score_cutoff:
+def best_match(name, dictionary):
+    """Find the best fuzzy match for name in the dictionary."""
+    names = [value["name"] for value in dictionary.values()]
+    best_match = process.extractOne(name, names)
+    if best_match[1] >= 90:  # if similarity is above 70
         return best_match[0]
+    else:
+        return None
+
+def is_substring(name1, name2):
+    """Check if name1 is a substring of name2 or vice versa."""
+    return name1.lower() in name2.lower() or name2.lower() in name1.lower()
+
+def find_substring_match(name, dictionary):
+    """Find a substring match for name in the dictionary."""
+    for value in dictionary.values():
+        if is_substring(name, value["name"]):
+            return value["name"]
     return None
 
 def name_associations(tidyhq, legacy, output_file):
@@ -32,35 +42,41 @@ def name_associations(tidyhq, legacy, output_file):
     for tidyhq_id, tidyhq_value in tidyhq_dict.items():
         tidyhq_name = tidyhq_value["name"]
         tidyhq_door = tidyhq_value.get('door', None)
-        legacy_name = best_match(tidyhq_name, legacy_dict)
+        legacy_name = best_match(tidyhq_name, legacy_dict) or find_substring_match(tidyhq_name, legacy_dict)
         if legacy_name:
             # Find the corresponding id in legacy_dict
             legacy_id = [id for id, value in legacy_dict.items() if value["name"] == legacy_name][0]
             legacy_door = legacy_dict[legacy_id].get('door', None)
-            matched_legacy_names.append(legacy_name)
-            if tidyhq_id == legacy_id:
-                id_match.append({
-                    "tidyhq_id": tidyhq_id, 
-                    "tidyhq_name": tidyhq_name, 
-                    "tidyhq_door": tidyhq_door,
-                    "legacy_id": legacy_id, 
-                    "legacy_name": legacy_name,
-                    "legacy_door": legacy_door
-                })
-            else:
-                id_different.append({
-                    "tidyhq_id": tidyhq_id, 
-                    "tidyhq_name": tidyhq_name, 
-                    "tidyhq_door": tidyhq_door,
-                    "legacy_id": legacy_id, 
-                    "legacy_name": legacy_name,
-                    "legacy_door": legacy_door
-                })
+        elif tidyhq_id in legacy_dict:
+            legacy_id = tidyhq_id
+            legacy_name = legacy_dict[legacy_id]["name"]
+            legacy_door = legacy_dict[legacy_id].get('door', None)
         else:
             tidyhq_without_match.append({
                 "tidyhq_id": tidyhq_id,
                 "tidyhq_name": tidyhq_name,
                 "tidyhq_door": tidyhq_door
+            })
+            continue
+
+        matched_legacy_names.append(legacy_name)
+        if tidyhq_id == legacy_id:
+            id_match.append({
+                "tidyhq_id": tidyhq_id, 
+                "tidyhq_name": tidyhq_name, 
+                "tidyhq_door": tidyhq_door,
+                "legacy_id": legacy_id, 
+                "legacy_name": legacy_name,
+                "legacy_door": legacy_door
+            })
+        else:
+            id_different.append({
+                "tidyhq_id": tidyhq_id, 
+                "tidyhq_name": tidyhq_name, 
+                "tidyhq_door": tidyhq_door,
+                "legacy_id": legacy_id, 
+                "legacy_name": legacy_name,
+                "legacy_door": legacy_door
             })
 
     # Find legacy names without a match in tidyhq
@@ -68,12 +84,18 @@ def name_associations(tidyhq, legacy, output_file):
         if legacy_value.get('door', 1) != 0:  # Ignore if door equals 0
             legacy_name = legacy_value["name"]
             legacy_door = legacy_value.get('door', None)
-            if legacy_name not in matched_legacy_names:
+            if legacy_name not in matched_legacy_names and not find_substring_match(legacy_name, tidyhq_dict):
                 legacy_without_match.append({
                     "legacy_id": legacy_id,
                     "legacy_name": legacy_name,
                     "legacy_door": legacy_door
                 })
+
+    # Sort each category by name
+    id_match = sorted(id_match, key=lambda x: x['tidyhq_name'])
+    id_different = sorted(id_different, key=lambda x: x['tidyhq_name'])
+    tidyhq_without_match = sorted(tidyhq_without_match, key=lambda x: x['tidyhq_name'])
+    legacy_without_match = sorted(legacy_without_match, key=lambda x: x['legacy_name'])
 
     # Write associations to output file
     associations = {"id_match": id_match, "id_different": id_different, 
