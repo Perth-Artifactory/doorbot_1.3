f5cd6e90667f487ef2a7c1b19b16270e376d597d
Blake Samuels <16077190+tazard@users.noreply.github.com>
2023-07-30 20:11:31 +0800

    Comments - correct

--- doorbot/utils/convert_old_rfid_key.py
+++ doorbot/utils/convert_old_rfid_key.py
@@ -3,7 +3,7 @@ The old Arduino based RFID reader would give hex values which don't
 match the number printed on the side of keys, nor what USB RFID reader
 reads. This script converts those hex values to the normal representation.
 
-All it does is drop the first 2 bytes (4 digits in hex).
+All it does is drop the first 1 bytes (2 digits in hex).
 """
 import os
 import argparse
@@ -16,11 +16,13 @@ def old_door_pi_hex_str_to_rfid(hex_str: str) -> int:
     decimal string printed on keyfobs and also what USB RFID 
     receiver outputs.
     """
-    # Chop off the first 1 hex chars (1 bytes). They don't
+    # Chop off the first 2 hex chars (1 bytes). They don't
     # appear in the hex conversion of the decimal value written
     # on keyfobs and output from USB RFID reader.
+    # USB RFID Reader gives 4 bytes where as keys actually have 5 bytes on them.
     hex_str = hex_str[2:]
     tag = int(hex_str, 16)
+    # Zero pad out to a 10 digit decimal number
     return f"{tag:0>10}"
 
 def convert(path, out):
